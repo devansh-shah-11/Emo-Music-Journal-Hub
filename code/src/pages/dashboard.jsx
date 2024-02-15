e6324d515a8e429dfc749f5d05e96f0824a64ade
Devansh Shah <devanshnshah11@gmail.com>
2024-02-15 13:55:04 +0530

    Logout and login working

--- code/src/pages/dashboard.jsx
+++ code/src/pages/dashboard.jsx
@@ -1,8 +1,60 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useContext } from 'react';
 import './dashboard.css';
+import axios from "axios";
+import { UserContext } from "../context/usercontext.jsx";
 
 function Dashboard(){
 
+    const { user } = useContext(UserContext);
+    const { logOutUser } = useContext(UserContext);
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const logOut = async () => {
+        try {
+            console.log("Logging out user: ", user);
+            const loggedOut = await logOutUser(user);
+            if (loggedOut) {
+                window.location.reload(true);
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+    const redirectNow = (path) => (e) => {
+        e.preventDefault();
+        window.location.href = path;
+    }
+
+    const SaveJournal = async () => {
+
+        console.log("Saving Journal...");
+        console.log(`Title: ${title}`);
+        console.log(`Body: ${body}`);
+
+        const journalEntry = {
+            title,
+            body
+        };
+        console.log(journalEntry);
+        try {
+            const response = await axios.post('http://localhost:3001/addentry', {
+                headers: {
+                    session_token: user,
+                },
+                params: {
+                    title: title,
+                    body: body
+                }
+            });
+            console.log("Response: ", response);
+        } catch (error) {
+            console.log("Error saving journal: ", error);
+        }
+    }
+    
     const [showSongs, setShowSongs] = useState(false);
 
     const videoRef = useRef(null);
@@ -23,22 +75,20 @@ function Dashboard(){
         <div class="dashboard">
             <div class="navbar">
                 <div class="navbar-content">
-                    <div class="app-name">Application Name</div>
+                    <div class="app-name">Smart Journal</div>
                     <div class="spacer"></div>
                         <div class="nav-items">
-                            <a href="#view_entries" style={{textDecoration: "none"}}>View Past Entries</a>
+                            <a href="/view_entry" onClick={redirectNow('/view_entry')} style={{textDecoration: "none"}}>View Past Entries</a>
                         </div>
-                        <div class="dropdown" style={{height: "90px"}}>
-                            <button class="dropbtn">Profile
-                                <i class="fa fa-caret-down"></i>
-                            </button>
-                            <div class="dropdown-content">
-                            <a href="#my_profile">My Profile</a>
-                            <a href="#logout">Logout 
-                                <i class="fa fa-power-off"></i>
-                            </a>
+                            <div class="dropdown" style={{height: "90px"}}>
+                                <button class="dropbtn">Profile
+                                    <i class="fa fa-caret-down"></i>
+                                </button>
+                                <div class="dropdown-content">
+                                <a href="/myprofile"  class="nav-link">My Profile</a>
+                                <a href="#logout"  class="nav-link" onClick={logOut}>Logout</a>
+                            </div>
                         </div>
-                    </div>
                 </div>
             </div>
             <br></br>
@@ -47,9 +97,20 @@ function Dashboard(){
             <div class="dashboard-content">
                 <div className="left-space"></div>
                 <div className="text-box">
-                    <input type="text" placeholder="Title" class="title-input" />
-                    <textarea placeholder="Body" class="body-input"></textarea>
-                    <button className="save-button">Save Journal</button>
+                    <input 
+                        type="text" 
+                        placeholder="Title" 
+                        class="title-input" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeholder="Body" 
+                        className="body-input" 
+                        value={body} 
+                        onChange={e => setBody(e.target.value)} 
+                    />
+                    <button className="save-button" onClick={SaveJournal}>Save Journal</button>
                 </div>
                 <div className="video-box">
                     <video ref={videoRef} id="live-video" autoPlay playsInline></video>
